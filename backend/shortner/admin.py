c382b940596c5ab9d3bc0bdbf480ce175bd1eb5d
amireho3ein <amireedoran@gmail.com>
2021-02-08 16:42:11 +0330

    add project

---
+++ backend/shortner/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from backend.shortner import models
+
+
+admin.site.register(models.Url)
---
+++ backend/shortner/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Url(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=8)
+    
+
+    def __str__(self):
+        return self.uuid
+        
