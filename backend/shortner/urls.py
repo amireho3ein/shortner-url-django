00b6bc1c797501e4d8c04800c185b20d1fd357ee
amireho3ein <amireedoran@gmail.com>
2021-02-08 17:56:31 +0330

    add views + url

--- backend/shortner/urls.py
+++ backend/shortner/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from backend.shortner import views
 
 urlpatterns = [
-    
+   path('', views.base, name='base'),
+   path('carete/', views.create, name='create'),
+   path('<str:pk>', views.get_url, name='get_url') 
 ]
--- backend/shortner/views.py
+++ backend/shortner/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Url
+import uuid
+from django.http import HttpResponse
 
-# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+
+def get_url(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect('https://'+url_details.link)
+    
+    
